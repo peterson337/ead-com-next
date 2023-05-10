f1c922b3ec10781a35b96b399d2263352369ab75
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-05-09 22:16:07 -0300

    Primeiro em desenvolvimento

--- src/pages/Cursos.tsx
+++ src/pages/Cursos.tsx
@@ -35,22 +35,24 @@ const Cursos = () => {
   }, [])
     
   return (
-    <div>
-        <h1 className='text-2xl bg-sky-500 text-white p-5'>Cursos Disponíveis:</h1>
-        {
-  cursos?.map((val) => {
-    return(
-      <div key={val.id} >
-     <a onClick={() => handleEditUser()}  className='p-5'
-     
-     >{val.id}</a>
-      </div>
-    )
-  }) ?? <div></div>
-}
-
+    <div className="flex flex-col h-screen">
+    <h1 className='text-2xl bg-[#0093ff] text-white p-5 '>Cursos Disponíveis:</h1>
+    <div className="container flex flex-col">
+      {cursos?.map((val) => (
+        <div key={val.id} className=' p-3   border-b border-gray-400 border-opacity-50'>
+          <a
+            onClick={() => handleEditUser()}
+            className='pl-3 text-[#0041ff] hover:text-[#f00] cursor-pointer hover:underline'>
+            {val.id}
+          </a>
+        </div>
+      ))}
     </div>
+  </div>
+  
   )
 }
-
+/* 
+04:06
+*/
 export default Cursos
